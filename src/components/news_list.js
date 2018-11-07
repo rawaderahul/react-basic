902780ad8efbf753b55ba2325980184ac1f4b4d5
Rohit <rohitrawade1212@gmail.com>
2018-11-07 18:29:09 +0530

    Initial commit

---
+++ src/components/news_list.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Components
+
+import Items from './news_list_item'
+
+const News = () =>{
+    return (
+        <div>
+            This is
+             News list Wrapper
+            <Items />
+        </div>
+    )
+}
+
+export default News;
+
---
+++ src/components/news_list_item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Items = () =>{
+    return <div> Itmes </div>    
+}
+
+export default Items;
